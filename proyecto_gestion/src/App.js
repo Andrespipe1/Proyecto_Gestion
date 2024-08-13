0801b354887fe4df9cfd6b54ec6d02668a52ef66
Andrespipe1 <andresftma@gmail.com>
2024-08-13 16:07:03 -0500

    prueba

--- proyecto_gestion/src/App.js
+++ proyecto_gestion/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          AYUDA POR QUE NO VALE
         </p>
         <a
           className="App-link"
