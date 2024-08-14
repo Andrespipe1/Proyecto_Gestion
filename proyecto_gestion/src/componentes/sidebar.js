0d9aadfda038d413e005fe74db1a3f5efbbbe149
Andrespipe1 <andresftma@gmail.com>
2024-08-13 23:01:25 -0500

    graficos

--- proyecto_gestion/src/componentes/sidebar.js
+++ proyecto_gestion/src/componentes/sidebar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
-import "./pagina_monto.css"
+import "./sidebar.css"
 import { useNavigate } from 'react-router-dom';
 
+
 const Sidebar = () => {
-    
+
     const navigate = useNavigate(); 
 
     const handlegraficoClick = () => {
@@ -18,17 +19,12 @@ const Sidebar = () => {
           <li><a >Ingresar Saldo</a></li>
           <li>Visualización:</li>
           <ul>
-            <li><a onClick={handlegraficoClick}>Gráficos</a></li>
+            <li><a href="" onClick={handlegraficoClick}>Gráficos</a></li>
             <li><a href="#listas">Listas</a></li>
           </ul>
           <li>Pagos:</li>
           <ul>
             <li><a href="#registrar">Registrar un pago</a></li>
-            <li>Visualización:</li>
-            <ul>
-              <li><a href="#graficos">Gráficos</a></li>
-              <li><a href="#listas">Listas</a></li>
-            </ul>
           </ul>
           <li>¡RECUERDA!</li>
           <p>De centavito en centavito se va llegando al dolarito</p>
